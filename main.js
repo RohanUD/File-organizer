d717ea148bb876f43fa3446aae4e9239db0ea5e1
RohanUD <78956997+RohanUD@users.noreply.github.com>
2021-12-05 15:21:59 +0530

    progress

--- main.js
+++ main.js
@@ -3,11 +3,21 @@
 
 // console.log(input);
 let bc = require("process");
+let fs = require("fs");
+let direction = require("path");
+const { dir } = require("console");
 var output = bc.argv;
 var Arr = output.slice(2);
 console.log(Arr); //input array
 
-// array would contain atmost 2elements 
+let typesOfFile={
+  media : ["mp4",'mkv'],
+  Pictures:['jpg','png','heic'],
+  documents:['pdf','doc','txt','ps','odf','odp','odg','xlsx','docx'],
+  app:['exe','dmg','pkg','deb']
+}
+
+// array would contain atmost 2elements
 
 let instruction = Arr[0];
 //mainly three types of instruction
@@ -17,30 +27,70 @@ let instruction = Arr[0];
 
 switch (instruction) {
   case "insert":
-      toInsert(Arr[1]);
+    toInsert(Arr[1]);
     break;
   case "organize":
-      organize(Arr[1]);
+    organize(Arr[1]);
     break;
   case "help":
-      help()
+    help();
     break;
   default:
     console.log("say a  proper command");
 }
 
-function toInsert(command) {
-    console.log('working fine');
-}
+function toInsert(command) {}
 function organize(command) {
-    console.log('working fine');
+  // first to make folder in the given path
+  //to identify all the files present in the folder
+  //to make mutliple folder for the given files
+
+  if (command === undefined) {
+    console.log("Enter the path");
+    return;
+  } else {
+    var pathValidator = fs.existsSync(command);
+    if (pathValidator === true) {
+      var desPth = direction.join(command, "organized-files");
+      if (fs.existsSync(desPth) == false) {
+        fs.mkdirSync(desPth);
+      }
+    } else {
+      console.log("Enter the correct path");
+    }
+  }
+
+  toOrganize(command, desPth);
+}
+
+function toOrganize(dirPath, desPath) {
+  var list = fs.readdirSync(dirPath);
+  // console.log(list);
+  for (let i = 0; i < list.length; i++) {
+    let fileAdress=direction.join(dirPath,list[i]);
+    let isItemsAreFileOrNot=fs.lstatSync(fileAdress).isFile();
+    if(isItemsAreFileOrNot==true){
+      let category=getcategory(list[i]);
+    }
+
+
+
+  }
 }
 
 function help() {
-    console.log(`
+  console.log(`
             Your avalaible commands are:
             insert 'path'
             organize 'path'
-            
+            help
     `);
+}
+
+function getcategory(filenames) {
+  let ext=direction.extname(filenames);
+  ext=ext.slice(1);
+  for(let key in typesOfFile){
+      let currentType=typesOfFile[key]
+  }
 }
